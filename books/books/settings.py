847989be1471bc4be0429c16e6ef05d4f7bba291
lorens <lorens32645@gmail.com>
2023-03-02 01:17:31 +0300

    gitigtnor pass for github

--- books/books/settings.py
+++ books/books/settings.py
@@ -9,11 +9,16 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
+import os, sys
 from pathlib import Path
-from passw import *
-
+# from dj_pomidor.passw import *
+# import sys
+# from sys.path.append("/mnt/c/python/dj_pomidor/passw.py") import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# import passw
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+# from passw import *
+from passw import *
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
