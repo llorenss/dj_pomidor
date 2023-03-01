59b1f9b19b2ce39363d7e5456c7726a5cee1b9d5
lorens <lorens32645@gmail.com>
2023-03-01 10:01:50 +0300

    filter search order

--- books/books/settings.py
+++ books/books/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store.apps.StoreConfig'
+    'store.apps.StoreConfig',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    ),
+    # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
--- books/store/models.py
+++ books/store/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=255, verbose_name="Название")
     price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Цена")
+    author_name = models.CharField(max_length=255, verbose_name="Автор")
+
     
     class Meta:
         verbose_name='Книги'
