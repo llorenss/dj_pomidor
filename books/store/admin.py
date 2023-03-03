6160854de4499b24028c2309c21e28e161a0f2d5
lorens <lorens32645@gmail.com>
2023-03-04 00:04:36 +0300

    model UserBookRelation

--- books/store/admin.py
+++ books/store/admin.py
@@ -1,7 +1,8 @@
+from django.contrib.admin import ModelAdmin
 from django.contrib import admin
-from store.models import Book
+from store.models import Book, UserBookRelation
 
-class BookAdmin(admin.ModelAdmin):
+class BookAdmin(ModelAdmin):
     # list_display = ('id','name','price')
     list_display = [field.name for field in Book._meta.get_fields()]
     # list_display = __all__
@@ -12,4 +13,9 @@ class BookAdmin(admin.ModelAdmin):
     # list_editable = ('name', 'price')
     list_filter = ('name','price')
     list_editable = [field.name for field in Book._meta.fields if field.name != "id"]
+
 admin.site.register(Book,BookAdmin)
+
+@admin.register(UserBookRelation)
+class UserBookRelation(ModelAdmin):
+    pass
--- books/store/models.py
+++ books/store/models.py
@@ -6,6 +6,7 @@ class Book(models.Model):
     price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Цена")
     author_name = models.CharField(max_length=255, verbose_name="Автор")
     owner = models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
+    readers = models.ManyToManyField(User, through='UserBookRelation')
 
     
     class Meta:
@@ -14,3 +15,21 @@ class Book(models.Model):
         # знак минус обратная сортировка
         # сортировка перейдёт в выдачу в браузер
         # ordering = ['-name']
+
+class UserBookRelation(models.Model):
+    RATE_CHOICES = (
+        (1,'a'),
+        (2,'b'),
+        (3,'c'),
+        (4,'d'),
+        (5,'e')
+    )
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    book = models.ForeignKey(Book,on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    in_bookmarks = models.BooleanField(default=False)
+    rate = models.PositiveBigIntegerField(choices=RATE_CHOICES)
+
+    def __str__(self):
+        return f'{self.user.username}:{self.book.name}, RATE {self.rate}'
+
