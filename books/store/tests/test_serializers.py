fcce3f601493db37657bb2f69d9adb2a7c9a59aa
lorens <lorens32645@gmail.com>
2023-03-03 05:09:42 +0300

    CRUD test

--- books/store/tests/test_serializers.py
+++ books/store/tests/test_serializers.py
@@ -5,19 +5,21 @@ from store.serializers import BooksSerializer
 
 class BookSerializerTestCase(TestCase):
     def test_ok(self):
-        book_1=Book.objects.create(name="testbook10", price=25)
-        book_2=Book.objects.create(name="testbook11", price=55)
-        data=BooksSerializer([book_2,book_1],many=True).data
+        book_1=Book.objects.create(name="testbook10", price=25, author_name='a')
+        book_2=Book.objects.create(name="testbook11", price=55, author_name='b')
+        data=BooksSerializer([book_1,book_2],many=True).data
         excepted_data = [
+               {
+            'id':book_1.id,
+            'name':'testbook10',
+            'price':'25.00',
+            'author_name':'a',
+            },
               {
             'id':book_2.id,
             'name':'testbook11',
-            'price':'55.00'
-            },
-            {
-            'id':book_1.id,
-            'name':'testbook10',
-            'price':'25.00'
+            'price':'55.00',
+            'author_name':'b',
             },
            
         ]
