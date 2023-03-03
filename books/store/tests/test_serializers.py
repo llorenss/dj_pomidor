ead695706abe83b93d703b0354eb6334fee37bb9
lorens <lorens32645@gmail.com>
2023-03-03 06:17:29 +0300

    permission and tests

--- books/store/tests/test_serializers.py
+++ books/store/tests/test_serializers.py
@@ -1,12 +1,15 @@
 from django.test import TestCase
 from store.models import Book
 from store.serializers import BooksSerializer
+from django.contrib.auth.models import User
 
 
 class BookSerializerTestCase(TestCase):
     def test_ok(self):
-        book_1=Book.objects.create(name="testbook10", price=25, author_name='a')
-        book_2=Book.objects.create(name="testbook11", price=55, author_name='b')
+        self.user_1 = User.objects.create(username='test_username')
+        self.user_2 = User.objects.create(username='test_username3')
+        book_1=Book.objects.create(name="testbook10", price=25, author_name='a',owner=self.user_1)
+        book_2=Book.objects.create(name="testbook11", price=55, author_name='b',owner=self.user_2)
         data=BooksSerializer([book_1,book_2],many=True).data
         excepted_data = [
                {
@@ -14,12 +17,14 @@ class BookSerializerTestCase(TestCase):
             'name':'testbook10',
             'price':'25.00',
             'author_name':'a',
+            'owner': book_1.owner.id 
             },
               {
             'id':book_2.id,
             'name':'testbook11',
             'price':'55.00',
             'author_name':'b',
+            'owner': book_2.owner.id 
             },
            
         ]
