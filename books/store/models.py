63553a5ea2d7791b319b463f1fa24386c411aa21
lorens <lorens32645@gmail.com>
2023-03-04 10:00:20 +0300

    like ... test

--- books/store/models.py
+++ books/store/models.py
@@ -5,16 +5,18 @@ class Book(models.Model):
     name = models.CharField(max_length=255, verbose_name="Название")
     price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Цена")
     author_name = models.CharField(max_length=255, verbose_name="Автор")
-    owner = models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
-    readers = models.ManyToManyField(User, through='UserBookRelation')
+    owner = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, related_name='my_books')
+    readers = models.ManyToManyField(User, through='UserBookRelation', related_name='books')
 
     
-    class Meta:
-        verbose_name='Книги'
-        verbose_name_plural='Книги'
+    # class Meta:
+    #     verbose_name='Книги'
+    #     verbose_name_plural='Книги'
         # знак минус обратная сортировка
         # сортировка перейдёт в выдачу в браузер
         # ordering = ['-name']
+    def __str__(self):
+        return f'Id {self.id}:{self.name}'
 
 class UserBookRelation(models.Model):
     RATE_CHOICES = (
@@ -28,7 +30,7 @@ class UserBookRelation(models.Model):
     book = models.ForeignKey(Book,on_delete=models.CASCADE)
     like = models.BooleanField(default=False)
     in_bookmarks = models.BooleanField(default=False)
-    rate = models.PositiveBigIntegerField(choices=RATE_CHOICES)
+    rate = models.PositiveBigIntegerField(choices=RATE_CHOICES, null=True)
 
     def __str__(self):
         return f'{self.user.username}:{self.book.name}, RATE {self.rate}'
--- books/store/views.py
+++ books/store/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
-from store.serializers import BooksSerializer
-from store.models import Book
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from store.serializers import BooksSerializer, UserBookRelationSerializer
+from store.models import Book, UserBookRelation
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly,IsAuthenticated
 from store.permissions import IsOwnerOrStaffOrReadOnly
+from rest_framework.mixins import UpdateModelMixin
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
@@ -19,6 +20,22 @@ class BookViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.validated_data['owner'] = self.request.user
         serializer.save()
+
+class UserBooksRelationView(UpdateModelMixin,GenericViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = UserBookRelation.objects.all()
+    serializer_class = UserBookRelationSerializer
+    lookup_field = 'book'
+
+    def get_object(self):
+        obj, _ = UserBookRelation.objects.get_or_create(user=self.request.user,
+                                                        book_id=self.kwargs['book'])
+        # просмотр при тестировании был
+        # obj, created = UserBookRelation.objects.get_or_create(user=self.request.user,
+        #                                                 book_id=self.kwargs['book'])
+        
+        # print('created', created)
+        return obj
 
 def auth(request):
     return render(request,'oauth.html')
