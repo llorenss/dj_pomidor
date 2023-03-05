51e55c64d2e233328da30e3b2a55fde545abd948
lorens <lorens32645@gmail.com>
2023-03-05 15:26:21 +0300

    debug_toolbar_force

--- books/books/settings.py
+++ books/books/settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "debug_toolbar_force.middleware.ForceDebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "books.urls"
